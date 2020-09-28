59c73990d50cb69d85e6326e3addbefc52d0c98f
Bew <Bew@sysops-MBP.broadband>
2020-09-28 11:37:23 +0100

    Adding thunk (sideEffect handling in React)

--- source/todos/reducer.js
+++ source/todos/reducer.js
@@ -1,4 +1,21 @@
-import { CREATE_TODO, REMOVED_TODO, COMPLETED_TODO } from './actions';
+import { CREATE_TODO, REMOVED_TODO, COMPLETED_TODO,
+         LOAD_TODOS_IN_PROGRESS, LOAD_TODOS_SUCCESS, LOAD_TODOS_FAILURE
+        } from './actions';
+
+export const isLoading = ( state=false, action ) => {
+    const { type } = action;
+
+    switch(type) {
+        case LOAD_TODOS_IN_PROGRESS:
+            return true;
+
+        case LOAD_TODOS_SUCCESS:
+        case LOAD_TODOS_FAILURE:
+            return false;
+        default:
+            return state;
+    }
+}
 
 export const todos = (state=[], action) => {
 
